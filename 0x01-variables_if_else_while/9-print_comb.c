70fd0ca3462d7d9182ceb7af2ed1c57acd1de59a
Dorcas Mhuka <dorcasmhuka90@gmail.com>
2023-02-17 08:29:58 -0800

    9 Alphabert

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,12 +1,12 @@
 /*
  * File: 9-print_comb.c
- * Auth: Dorcas mhuka
+ * Auth: Dorcas Mhuka
  */
 
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of single-digit numbers.
+ * main - Prints all possible combinations of single-digit numbers
  *
  * Return: Always 0.
  */
@@ -19,7 +19,7 @@ int main(void)
 		putchar((num % 10) + '0');
 		if (num == 9)
 			continue;
-		
+
 		putchar(',');
 		putchar(' ');
 	}
@@ -27,4 +27,4 @@ int main(void)
 	putchar('\n');
 
 	return (0);
-}
+	}
